c9766b4598493d9ab3832529d10051c8055a151c
liuche <706716852@qq.com>
2023-01-17 18:03:48 +0800

    使用idea中hot-fix第一次提交

--- src/main/java/com/liuche/git/GitTest.java
+++ src/main/java/com/liuche/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello World");
         System.out.println("Hello LiuZuChe");
         System.out.println("Hello Movie");
+        System.out.println("Hello hot-fix");
     }
 }
