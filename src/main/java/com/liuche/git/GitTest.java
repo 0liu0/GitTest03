050c692784a659ea30ea14594b01f0fb093e59a2
liuche <706716852@qq.com>
2023-01-17 14:30:57 +0800

    使用idea第二次提交

--- src/main/java/com/liuche/git/GitTest.java
+++ src/main/java/com/liuche/git/GitTest.java
@@ -3,5 +3,6 @@ package com.liuche.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello World");
     }
 }
